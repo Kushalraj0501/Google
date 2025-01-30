efeda0690045d30fb049097079a55b14f2edaa71
Firstgit <kusha@DESKTOP-UKDLUAF>
2025-01-31 00:44:29 +0530

    Updated

---
+++ src/main/java/git_jenkins/jenkins/App.java
@@ -0,0 +1,10 @@
+package git_jenkins.jenkins;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/git_jenkins/jenkins/open_url.java
@@ -0,0 +1,33 @@
+package git_jenkins.jenkins;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class open_url {
+
+	 public static void main(String[] args) {
+	        // Set the system property for ChromeDriver (path to chromedriver executable)
+	       
+
+	        // Create an instance of ChromeDriver (launch the Chrome browser)
+	        WebDriver driver = new ChromeDriver();
+
+	        try {
+	            // Navigate to the desired website (GeeksforGeeks in this example)
+	            driver.get("https://www.geeksforgeeks.org/");
+
+	            // Get and print the page title
+	            String pageTitle = driver.getTitle();
+	            System.out.println("Page Title: " + pageTitle);
+
+	            // Wait for a few seconds (for demonstration purposes only)
+	            Thread.sleep(3000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        } finally {
+	            // Close the browser
+	            driver.quit();
+	        }
+	    }
+	
+}
